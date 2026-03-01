82cf3b3661c1c9f02a04e06d6a1f754a825cc7af
Zakari <zakari.adamu714@gmail.com>
2026-03-01 23:17:25 +0100

    fix(nav): make sign-in link route respond correctly

--- app/(root)/sign-in.tsx
+++ app/(root)/sign-in.tsx
@@ -1,29 +1,17 @@
-import { Image } from "expo-image";
-import React, { ReactElement } from "react";
-import {
-  Alert,
-  Dimensions,
-  ScrollView,
-  Text,
-  TouchableOpacity,
-  View,
-} from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { Image } from 'expo-image';
+import React, { ReactElement } from 'react';
+import { Alert, Dimensions, ScrollView, Text, TouchableOpacity, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-import icons from "@/constants/icons";
-import images from "@/constants/images";
-import { login } from "@/lib/appwrite";
-import { useGlobalContext } from "@/lib/global-provider";
-import { Redirect } from "expo-router";
+import icons from '@/constants/icons';
+import images from '@/constants/images';
+import { login } from '@/lib/appwrite';
+import { useGlobalContext } from '@/lib/global-provider';
 
 const SignIn = (): ReactElement => {
-  const screenHeight = Dimensions.get("window").height;
+  const screenHeight = Dimensions.get('window').height;
 
-  const { refetch, loading, isLoggedIn } = useGlobalContext();
-
-  if (!loading && isLoggedIn) {
-    return <Redirect href="/" />;
-  }
+  const { refetch } = useGlobalContext();
 
   const handleLogin = async () => {
     const result = await login();
@@ -31,10 +19,7 @@ const SignIn = (): ReactElement => {
     if (result) {
       refetch({});
     } else {
-      Alert.alert(
-        "Login Failed",
-        "Unable to login with Google. Please try again.",
-      );
+      Alert.alert('Login Failed', 'Unable to login with Google. Please try again.');
     }
   };
 
@@ -43,7 +28,7 @@ const SignIn = (): ReactElement => {
       <ScrollView contentContainerClassName="h-full">
         <Image
           source={images.onboarding}
-          style={{ width: "100%", height: screenHeight * 0.6 }}
+          style={{ width: '100%', height: screenHeight * 0.6 }}
           contentFit="contain"
         />
 
@@ -52,23 +37,17 @@ const SignIn = (): ReactElement => {
             Welcome to AISVILLE
           </Text>
           <Text className="mt-2 text-center font-rubik-bold text-2xl text-black-300">
-            Let&apos;s Get You Closer to {"\n"}
+            Let&apos;s Get You Closer to {'\n'}
             <Text className="text-primary-300">Your Ideal Home</Text>
           </Text>
-          <Text className="font-rubik mt-5 text-center">
-            Login to AisVille with Google
-          </Text>
+          <Text className="font-rubik mt-5 text-center">Login to AisVille with Google</Text>
 
           <TouchableOpacity
             onPress={handleLogin}
             className="mt-5 w-full rounded-full bg-white py-4 shadow-md shadow-zinc-300"
           >
             <View className="flex flex-row items-center justify-center">
-              <Image
-                source={icons.google}
-                style={{ width: 20, height: 20 }}
-                contentFit="contain"
-              />
+              <Image source={icons.google} style={{ width: 20, height: 20 }} contentFit="contain" />
               <Text className="ml-2 font-rubik-medium text-lg text-black-300">
                 Continue with Google
               </Text>
