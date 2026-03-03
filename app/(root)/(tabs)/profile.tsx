8bdc0fe880fc290b2b314dd7b5cd695a325abd76
Zakari <zakari.adamu714@gmail.com>
2026-03-03 15:31:35 +0100

    feat(auth): completed the auth flow by implementing the logout functionality in the profile screen

--- app/(root)/(tabs)/profile.tsx
+++ app/(root)/(tabs)/profile.tsx
@@ -1,12 +1,94 @@
-import React from "react";
-import { Text, View } from "react-native";
+import { settings } from '@/constants/data';
+import icons from '@/constants/icons';
+import { logout } from '@/lib/appwrite';
+import { useGlobalContext } from '@/lib/global-provider';
+import { Image } from 'expo-image';
+import React from 'react';
+import { Alert, ScrollView, Text, TouchableOpacity, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-const profile = () => {
-	return (
-		<View>
-			<Text>profile</Text>
-		</View>
-	);
+interface SettingsItemProps {
+  title: string;
+  icon: any;
+  onPress?: () => void;
+  textStyle?: string;
+  showArrow?: boolean;
+}
+
+const SettingsItem = ({ title, icon, onPress, textStyle, showArrow = true }: SettingsItemProps) => {
+  return (
+    <TouchableOpacity className="flex flex-row items-center justify-between py-3" onPress={onPress}>
+      <View className="flex flex-row items-center justify-between gap-3">
+        <Image source={icon} style={{ width: 20, height: 20 }} contentFit="contain" />
+        <Text className={`font-rubik-medium text-lg text-black-300 ${textStyle}`}>{title}</Text>
+      </View>
+      {showArrow && (
+        <Image source={icons.rightArrow} style={{ width: 16, height: 16 }} contentFit="contain" />
+      )}
+    </TouchableOpacity>
+  );
 };
 
-export default profile;
+const Profile = () => {
+  const { user, refetch } = useGlobalContext();
+
+  const handleLogout = async () => {
+    const response = await logout();
+    if (response) {
+      Alert.alert('Logout Successful', 'You have been logged out successfully.');
+      refetch({});
+    } else {
+      Alert.alert('Logout Failed', 'An error occurred while logging out. Please try again.');
+    }
+  };
+  return (
+    <SafeAreaView className="h-full bg-white">
+      <ScrollView showsVerticalScrollIndicator={false} contentContainerClassName="pb-32 px-7">
+        <View className="mt-5 flex flex-row items-center justify-between">
+          <Text className="font-rubik-bold text-xl text-black-300">Profile</Text>
+          <Image source={icons.bell} style={{ width: 20, height: 20 }} contentFit="contain" />
+        </View>
+        <View className="mt-5 flex flex-row justify-center">
+          <View className="relative mt-5 flex flex-col items-center">
+            <Image
+              source={{
+                uri:
+                  user?.avatar ||
+                  'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png',
+              }}
+              style={{ width: 80, height: 80, borderRadius: 40, position: 'relative' }}
+              contentFit="contain"
+            />
+            <TouchableOpacity className="absolute bottom-11 right-4">
+              <Image source={icons.edit} style={{ width: 9, height: 9 }} contentFit="contain" />
+            </TouchableOpacity>
+            <Text className="mt-3 font-rubik-semi-bold text-base text-black-300">{user?.name}</Text>
+          </View>
+        </View>
+
+        {/* settings item */}
+        <View className="mt-10 flex flex-col">
+          <SettingsItem title="My Bookings" icon={icons.calendar} onPress={() => {}} />
+          <SettingsItem title="Payments" icon={icons.wallet} onPress={() => {}} />
+        </View>
+        <View className="mt-5 flex flex-col border-t border-primary-200 pt-5">
+          {settings.slice(2).map((item, index) => (
+            <SettingsItem key={index} {...item} />
+          ))}
+        </View>
+
+        <View className="mt-5 flex flex-col border-t border-primary-200 pt-5">
+          <SettingsItem
+            title="Logout"
+            icon={icons.logout}
+            onPress={handleLogout}
+            textStyle="text-danger"
+            showArrow={false}
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default Profile;
