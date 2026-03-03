ba84fb728fcd4e4bd661b4a77b46da0a7553421e
Zakari <zakari.adamu714@gmail.com>
2026-03-03 17:42:12 +0100

    fix: resolve TS any type with an appropriate type declaration

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -12,28 +12,13 @@ export default function Index() {
     userProfile?.email?.split('@')[0] ||
     'User';
 
-  // const handleLogout = async () => {
-  //   const response = await logout();
-  //   if (response) {
-  //     Alert.alert('Logout Successful', 'You have been logged out successfully.');
-  //     return <Redirect href="/" />;
-  //   } else {
-  //     Alert.alert('Logout Failed', 'An error occurred while logging out. Please try again.');
-  //   }
-  // };
-
   return (
     <View className="flex-1 items-center justify-center gap-4 px-6">
       <Text className="text-center font-rubik-bold text-3xl font-bold text-black-300">
         Welcome to AisVille
       </Text>
       {isLoggedIn ? (
-        <>
-          <Text className="text-base font-medium text-blue-600">Welcome {displayName}</Text>
-          {/* <Pressable className="rounded-md bg-blue-600 px-4 py-2" onPress={handleLogout}>
-            <Text className="font-semibold text-white">Logout</Text>
-          </Pressable> */}
-        </>
+        <Text className="text-base font-medium text-blue-600">Welcome {displayName}</Text>
       ) : (
         <Link href="/sign-in" asChild>
           <Pressable className="rounded-md bg-blue-600 px-4 py-2">
